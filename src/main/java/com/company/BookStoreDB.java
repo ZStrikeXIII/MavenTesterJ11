43d6763c6996e685608c1bac36584542e9d0e27d
ZStrikeXIII <jacksonturner1999@gmail.com>
2020-02-20 21:52:57 -0500

    Iteration 2.2 Update: Added GSON and JSON parsing abilities, along with a JSON file. Added BookStoreDB class and BookStore.db to get closer to a complete iteration.

---
+++ src/main/java/com/company/BookStoreDB.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ * At the current moment, this class has no functionality, but it will involve the BookStore
+ * database file in some way. Later iterations will most likely contain the rest of the code needed
+ * to both connect the database and alter it in some way, though at the current moment it does
+ * nothing.
+ */
+public class BookStoreDB {
+
+}
